8714db89384bd57996ccc806f019ac48bbd07c20
DeniRizki15 <armandogaming1@gmail.com>
2026-05-08 15:57:29 +0700

    Update get-donations.js

--- api/sociabuzz/get-donations.js
+++ api/sociabuzz/get-donations.js
@@ -1,11 +1,22 @@
-import { donations } from "../../lib/store.js";
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+// Ambil dari global storage
+if (!global._donations) {
+  global._donations = [];
+}
 
 export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
   if (req.method !== "GET") {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
-  const unprocessed = donations.filter(d => !d.processed);
+  const unprocessed = global._donations.filter(d => !d.processed);
   unprocessed.forEach(d => (d.processed = true));
 
   return res.status(200).json({
